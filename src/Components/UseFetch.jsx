2f3d9332a9334e83bfe3503aa0b64e8f3e19696a
biswajitjena107 <biswajitjena10799910@gmail.com>
2024-11-12 15:02:54 +0530

    New Commit

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,14 @@
+import { useState,useEffect } from "react";
+
+
+
 const UseFetch = (url) => {
-    
+   const[data,setdata] = useState();
+    useEffect(() => {
+      fetch(url).then((res) => res.json())
+      .then((data) => setdata(data))
+    },[])
+    return [data]
 }
 
 export default UseFetch
